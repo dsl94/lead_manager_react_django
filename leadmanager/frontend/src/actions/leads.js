be381b0b6cfa20f9ad76bb2a63808c32151bd9e9
nemanja.petrovic <nemanjabosnjane@gmail.com>
2019-09-24 09:54:55 +0200

    Get and delete implemented

---
+++ leadmanager/frontend/src/actions/leads.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import {GET_LEADS, DELETE_LEAD} from "./types";
+
+// GET LEADS Action method
+export const getLeads = () => dispatch =>{
+    axios.get('/api/leads/')
+        .then(res => {
+           dispatch({
+              type: GET_LEADS,
+               payload: res.data
+           });
+        }).catch(err => console.log(err));
+};
+
+// DELETE LEADS Action method
+export const deleteLead = (id) => dispatch =>{
+    axios.delete(`/api/leads/${id}/`)
+        .then(res => {
+           dispatch({
+              type: DELETE_LEAD,
+               payload: id
+           });
+        }).catch(err => console.log(err));
+};
